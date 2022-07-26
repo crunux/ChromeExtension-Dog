34ec791ef2144fd85f1b51ab9ef08bc3eea2d001
Cross <joancruz0502@hotmail.com>
2022-07-25 23:43:34 -0400

    chromeExtension-Dog:v1.1

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // Developer by Cross-Dev  23/07/22
 const nextBtn = document.querySelector('.btn');
 const imagen = document.querySelector('.dogs')
+const breed = document.querySelector('.breed')
 
 const API_URL = "https://api.thedogapi.com/v1/images/search";
 
@@ -20,6 +21,7 @@ const fetchdogs = async () => {
         const dog = await response.json()
         console.log(dog)
         imagen.setAttribute('src', dog[0].url)
+        breed.innerHTML = dog[0].breeds[0].name;
     } catch (error) {
         console.log(error);
     }
